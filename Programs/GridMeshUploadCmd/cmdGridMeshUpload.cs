2d90e309e0b329906d4274ed4cb5b71769bb06c6
Jak Daniels <jak@ateb.co.uk>
2015-09-10 10:53:02 +0100

    Add GridMeshUploadCmd project

---
+++ Programs/GridMeshUploadCmd/cmdGridMeshUpload.cs
@@ -0,0 +1,296 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+using OpenMetaverse;
+using OpenMetaverse.Http;
+using OpenMetaverse.Imaging;
+using OpenMetaverse.ImportExport;
+using OpenMetaverse.StructuredData;
+
+namespace cmdGridMeshUpload
+{
+    public class cmdGridMeshUpload
+    {
+
+        private GridClient Client;
+        private string FileName = String.Empty;
+        private string LoginURL = String.Empty;
+        private string FirstName = String.Empty;
+        private string LastName = String.Empty;
+        private string Password = String.Empty;
+        private bool UploadImages = true;
+        private bool NoResize = false;
+        private bool ShowUsage = false;
+        private int Debug = 0;
+
+        private bool Connected = false;
+        private bool ConnectFailed = false;
+        private bool EventQueueRunning = false;
+        private bool UploadComplete = false;
+        private bool UploadFailed = false;
+
+        private UUID UploadedMeshUUID;
+
+        public static int Main(string[] args)
+        {
+            //args should be --login=LoginURL --first=FirstName --last=LastName ==password=Password --file=PathToMesh [--cc-first=CopyToFirstName] [--cc-last=CopyToLastName] [--upload-images] [--no-resize]
+
+            cmdGridMeshUpload m = new cmdGridMeshUpload();
+
+            Dictionary<string, string> arg = m.ParseArgs(args);
+
+            if (arg.ContainsKey("login"))
+            {
+                m.LoginURL = arg["login"];
+            }
+            else
+            {
+                m.ShowUsage = true;
+            }
+
+            if (arg.ContainsKey("first"))
+            {
+                m.FirstName = arg["first"];
+            }
+            else
+            {
+                m.ShowUsage = true;
+            }
+
+            if (arg.ContainsKey("last"))
+            {
+                m.LastName = arg["last"];
+            }
+            else
+            {
+                m.ShowUsage = true;
+            }
+
+            if (arg.ContainsKey("password"))
+            {
+                m.Password = arg["password"];
+            }
+            else
+            {
+                m.ShowUsage = true;
+            }
+
+            if (arg.ContainsKey("file"))
+            {
+                m.FileName = arg["file"];
+            }
+            else
+            {
+                m.ShowUsage = true;
+            }
+
+            if (arg.ContainsKey("upload-images"))
+            {
+                m.UploadImages = true;
+            }
+            if (arg.ContainsKey("no-resize"))
+            {
+                m.NoResize = true;
+            }
+            
+            if (arg.ContainsKey("d")) Int32.TryParse(arg["d"], out m.Debug);
+            else if(arg.ContainsKey("debug")) Int32.TryParse(arg["debug"], out m.Debug);
+            
+            if (m.ShowUsage)
+            {
+                Console.WriteLine("Usage: GridMeshUploadCmd.exe --login=LoginURL --first=FirstName --last=LastName --password=Password --file=PathToMesh [--upload-images] [--no-resize]");
+                return 1;
+            }
+
+            m.InitClient();
+            //initiate login
+            LoginParams lp = m.Client.Network.DefaultLoginParams(m.FirstName, m.LastName, m.Password, "GridMeshUploadCmd", "0.1");
+            lp.URI = m.LoginURL;
+            m.Client.Network.BeginLogin(lp);
+
+            while (!m.Connected)
+            {
+                if (m.ConnectFailed) return 1;
+                System.Threading.Thread.Sleep(500);
+            }
+
+
+            while (!m.EventQueueRunning) System.Threading.Thread.Sleep(500);
+
+            if (m.UploadMesh())
+            {
+                while (!m.UploadComplete) System.Threading.Thread.Sleep(500);
+
+            }
+            else
+            {
+                Console.WriteLine("Error Uploading Mesh");
+            }
+
+            //initiate logout
+            if (m.Connected)
+            {
+                Console.WriteLine("Logging Out");
+                m.Client.Network.Logout();
+            }
+            m.Client = null;
+            if (m.UploadFailed) return 1; else return 0;
+        }
+
+        private Dictionary<string, string> ParseArgs(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            int i = 0;
+            string a;
+            string v = "";
+            int ov = 0;
+
+            while (i < args.Length)
+            {
+                if (args[i].Substring(0, 2) == "--") a = args[i].Substring(2, args[i].Length - 2);
+                else
+                {
+                    if (args[i].Substring(0, 1) == "-") a = args[i].Substring(1, args[i].Length - 1);
+                    else
+                    {
+                        a = String.Format("{0}", ov);
+                        ov++;
+                    }
+                }
+                if (a.Contains("="))
+                {
+                    string[] tmp = a.Split("=".ToCharArray());
+                    //Console.WriteLine("{0}:{1}", tmp[0], tmp[1]);
+                    dictionary.Add(tmp[0], tmp[1]);
+                }
+                else
+                {
+                    if (args[i].Substring(0, 1) == "-" || i == args.Length) v = "1";
+                    else v = args[i];
+                    //Console.WriteLine("{0}:{1}", a, v);
+                    dictionary.Add(a, v);
+                }
+                i++;
+            }
+            return dictionary;
+        }
+
+        private void InitClient()
+        {
+            Client = new GridClient();
+            Client.Network.EventQueueRunning += Network_OnEventQueueRunning;
+            Client.Network.LoginProgress += Network_OnLogin;
+
+            // Turn almost everything off since we are only interested in uploading textures
+            Settings.LOG_LEVEL = Helpers.LogLevel.None;
+            Client.Settings.ALWAYS_DECODE_OBJECTS = false;
+            Client.Settings.ALWAYS_REQUEST_OBJECTS = false;
+            Client.Settings.SEND_AGENT_UPDATES = true;
+            Client.Settings.OBJECT_TRACKING = false;
+            Client.Settings.STORE_LAND_PATCHES = false;
+            Client.Settings.MULTIPLE_SIMS = false;
+            Client.Self.Movement.Camera.Far = 32.0f;
+            Client.Throttle.Cloud = 0.0f;
+            Client.Throttle.Land = 0.0f;
+            Client.Throttle.Wind = 0.0f;
+
+            Client.Throttle.Texture = 446000.0f;
+        }
+
+        private void Network_OnLogin(object sender, LoginProgressEventArgs e)
+        {
+
+            if (e.Status == LoginStatus.Success)
+            {
+                Connected = true;
+            }
+            else if (e.Status == LoginStatus.Failed)
+            {
+                Console.WriteLine("Error logging in ({0}): {1}", Client.Network.LoginErrorKey, Client.Network.LoginMessage);
+                ConnectFailed = true;
+            }
+
+        }
+
+        private void Network_OnEventQueueRunning(object sender, EventQueueRunningEventArgs e)
+        {
+            //Console.WriteLine("Event queue is running for {0}, enabling uploads");
+            EventQueueRunning = true;
+        }
+
+        private bool UploadMesh()
+        {
+            Console.WriteLine("Processing: {0}", FileName);
+
+            var parser = new cmdColladaLoader();
+            parser.Debug = Debug;
+            parser.Resize = !NoResize;
+            var prims = parser.Load(FileName, UploadImages);
+            if (prims == null || prims.Count == 0)
+            {
+                Console.WriteLine("Error: Failed to parse collada file");
+                return false;
+            }
+
+            Console.WriteLine("Parse collada file success, found {0} objects", prims.Count);
+
+            Console.WriteLine("Uploading...");
+
+            string ObjectName=Path.GetFileNameWithoutExtension(FileName);
+
+            UUID ObjectFolder = Client.Inventory.FindFolderForType(AssetType.Object);
+            UUID MyObjectFolder = Client.Inventory.CreateFolder(ObjectFolder, ObjectName);
+            UUID MyTexturesFolder = Client.Inventory.CreateFolder(MyObjectFolder, "Textures");
+
+            Console.WriteLine("Created New Folder under 'Objects' called '{0}' with UUID {1}", ObjectName, MyObjectFolder);
+            Console.WriteLine("Created New Folder under '{0}' called 'Textures' with UUID {1}", ObjectName, MyTexturesFolder);
+            
+            var uploader = new cmdModelUploader(Client, prims);
+            var uploadDone = new AutoResetEvent(false);
+
+            uploader.IncludePhysicsStub = true;
+            uploader.UseModelAsPhysics = false;
+            uploader.InvName = ObjectName;
+            uploader.InvDescription = "Uploaded by GridUploadMeshCmd on " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            uploader.InvAssetFolderUUID = MyObjectFolder;
+            uploader.InvTextureFolderUUID = MyTexturesFolder;
+            uploader.UploadTextures = UploadImages;
+            uploader.Debug = Debug;
+
+            uploader.Upload((res=>
+
+            {
+                if (res == null)
+                {
+                    Console.WriteLine("Upload failed.");
+                    UploadComplete = true;
+                    UploadFailed = true;
+                }
+                else
+                {
+                    UploadedMeshUUID = uploader.ReturnedMeshUUID;
+                    Console.WriteLine("Upload success. Uploaded Mesh has Asset UUID: {0}", UploadedMeshUUID);
+                    UploadComplete = true;
+                }
+
+                uploadDone.Set();
+            }));
+
+            if (!uploadDone.WaitOne(4 * 60 * 1000))
+            {
+                Console.WriteLine("Message upload timeout");
+                UploadComplete = true;
+                UploadFailed = true;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Programs/GridMeshUploadCmd/cmdModelUploader.cs
@@ -0,0 +1,273 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+using OpenMetaverse;
+using OpenMetaverse.Http;
+using OpenMetaverse.Imaging;
+using OpenMetaverse.ImportExport;
+using OpenMetaverse.StructuredData;
+
+
+namespace OpenMetaverse.ImportExport
+{
+    
+    /// <summary>
+    /// Implements mesh upload communications with the simulator
+    /// </summary>
+    public class cmdModelUploader
+    {
+        public GridClient Client;
+        public List<ModelPrim> Prims;
+        public List<byte[]> Images;
+        public Dictionary<string, int> ImgIndex;
+        public string InvName = "NewMesh";
+        public string InvDescription = "";
+        public UUID InvAssetFolderUUID, InvTextureFolderUUID;
+        public bool UploadTextures;
+        public int Debug;
+        public UUID ReturnedMeshUUID;
+
+        /// <summary>
+        /// Inlcude stub convex hull physics, required for uploading to Second Life
+        /// </summary>
+        public bool IncludePhysicsStub;
+
+        /// <summary>
+        /// Use the same mesh used for geometry as the physical mesh upload
+        /// </summary>
+        public bool UseModelAsPhysics;
+
+        /// <summary>
+        /// Callback for mesh upload operations
+        /// </summary>
+        /// <param name="result">null on failure, result from server on success</param>
+        public delegate void ModelUploadCallback(OSD result);
+
+        /// <summary>
+        /// Creates instance of the mesh uploader
+        /// </summary>
+        /// <param name="client">GridClient instance to communicate with the simulator</param>
+        /// <param name="prims">List of ModelPrimitive objects to upload as a linkset</param>
+        public cmdModelUploader(GridClient client, List<ModelPrim> prims)
+        {
+            this.Client = client;
+            this.Prims = prims;
+        }
+        /// <summary>
+        /// Performs model upload in one go, without first checking for the price
+        /// </summary>
+        /// <param name="callback">Callback that will be invoke upon completion of the upload. Null is sent on request failure</param>
+        public void Upload(ModelUploadCallback callback)
+        {
+            PrepareUpload((result =>
+            {
+                if (result == null && callback != null)
+                {
+                    callback(null);
+                    return;
+                }
+
+                if (result is OSDMap)
+                {
+                    var res = (OSDMap)result;
+                    Uri uploader = new Uri(res["uploader"]);
+                    PerformUpload(uploader, (contents =>
+                    {
+                        if (contents != null)
+                        {
+                            var reply = (OSDMap)contents;
+                            if (reply.ContainsKey("new_inventory_item") && reply.ContainsKey("new_asset"))
+                            {
+                                // Request full update on the item in order to update the local store
+                                Client.Inventory.RequestFetchInventory(reply["new_inventory_item"].AsUUID(), Client.Self.AgentID);
+                                ReturnedMeshUUID = reply["new_asset"].AsUUID();
+                            }
+                        }
+                        if (callback != null) callback(contents);
+                    }));
+                }
+            }));
+
+        }
+
+        /// <summary>
+        /// Ask server for details of cost and impact of the mesh upload
+        /// </summary>
+        /// <param name="callback">Callback that will be invoke upon completion of the upload. Null is sent on request failure</param>
+        public void PrepareUpload(ModelUploadCallback callback)
+        {
+            Uri url = null;
+            if (Client.Network.CurrentSim == null ||
+                Client.Network.CurrentSim.Caps == null ||
+                null == (url = Client.Network.CurrentSim.Caps.CapabilityURI("NewFileAgentInventory")))
+            {
+                Console.WriteLine("Cannot upload mesh, no connection or NewFileAgentInventory not available");
+                if (callback != null) callback(null);
+                return;
+            }
+
+            Images = new List<byte[]>();
+            ImgIndex = new Dictionary<string, int>();
+
+            OSDMap req = new OSDMap();
+            req["name"] = InvName;
+            req["description"] = InvDescription;
+
+            req["asset_resources"] = AssetResources(UploadTextures);
+            req["asset_type"] = "mesh";
+            req["inventory_type"] = "object";
+
+            req["folder_id"] = InvAssetFolderUUID;
+            req["texture_folder_id"] = InvTextureFolderUUID;
+
+            req["everyone_mask"] = (int)PermissionMask.All;
+            req["group_mask"] = (int)PermissionMask.All; ;
+            req["next_owner_mask"] = (int)PermissionMask.All;
+
+            CapsClient request = new CapsClient(url);
+            request.OnComplete += (client, result, error) =>
+            {
+                if (error != null || result == null || result.Type != OSDType.Map)
+                {
+                    Console.WriteLine("Mesh upload request failure: {0}", error.Message);
+                    if (callback != null) callback(null);
+                    return;
+                }
+                OSDMap res = (OSDMap)result;
+
+                if (res["state"] != "upload")
+                {
+                    Console.WriteLine("Mesh upload failure: {0}", res["message"]);
+                    if (callback != null) callback(null);
+                    return;
+                }
+
+                if (Debug > 2) Console.WriteLine("Response from mesh upload prepare:\n{0}", OSDParser.SerializeLLSDNotationFormatted(result));
+                if (callback != null) callback(result);
+            };
+
+            if (Debug > 2) Console.WriteLine("Sending Request Resources: {0}", OSDParser.SerializeLLSDNotationFormatted(req));
+            request.BeginGetResponse(req, OSDFormat.Xml, 3 * 60 * 1000);
+
+        }
+
+        OSD AssetResources(bool upload)
+        {
+            OSDArray instanceList = new OSDArray();
+            List<byte[]> meshes = new List<byte[]>();
+            //List<byte[]> textures = new List<byte[]>();
+
+            foreach (var prim in Prims)
+            {
+                OSDMap primMap = new OSDMap();
+
+                OSDArray faceList = new OSDArray();
+
+                foreach (var face in prim.Faces)
+                {
+                    OSDMap faceMap = new OSDMap();
+
+                    faceMap["diffuse_color"] = face.Material.DiffuseColor;
+                    faceMap["fullbright"] = false;
+
+                    if (face.Material.TextureData != null)
+                    {
+                        int index;
+                        if (ImgIndex.ContainsKey(face.Material.Texture))
+                        {
+                            index = ImgIndex[face.Material.Texture];
+                        }
+                        else
+                        {
+                            index = Images.Count;
+                            ImgIndex[face.Material.Texture] = index;
+                            Images.Add(face.Material.TextureData);
+                        }
+                        faceMap["image"] = index;
+                        faceMap["scales"] = 1.0f;
+                        faceMap["scalet"] = 1.0f;
+                        faceMap["offsets"] = 0.0f;
+                        faceMap["offsett"] = 0.0f;
+                        faceMap["imagerot"] = 0.0f;
+                    }
+
+                    faceList.Add(faceMap);
+                }
+
+                primMap["face_list"] = faceList;
+
+                primMap["position"] = prim.Position;
+                primMap["rotation"] = prim.Rotation;
+                primMap["scale"] = prim.Scale;
+
+                primMap["material"] = (int)Material.Wood; // always sent as "wood" material
+                primMap["physics_shape_type"] = (int)PhysicsShapeType.ConvexHull; // always sent as "convex hull";
+                primMap["mesh"] = meshes.Count;
+                meshes.Add(prim.Asset);
+
+                instanceList.Add(primMap);
+            }
+
+            OSDMap resources = new OSDMap();
+            resources["instance_list"] = instanceList;
+
+            OSDArray meshList = new OSDArray();
+            foreach (var mesh in meshes)
+            {
+                meshList.Add(OSD.FromBinary(mesh));
+            }
+            resources["mesh_list"] = meshList;
+
+            OSDArray textureList = new OSDArray();
+            for (int i = 0; i < Images.Count; i++)
+            {
+                if (upload)
+                {
+                    textureList.Add(new OSDBinary(Images[i]));
+                }
+                else
+                {
+                    textureList.Add(new OSDBinary(Utils.EmptyBytes));
+                }
+            }
+
+            resources["texture_list"] = textureList;
+
+            resources["metric"] = "MUT_Unspecified";
+            return resources;
+        }
+
+        /// <summary>
+        /// Performas actual mesh and image upload
+        /// </summary>
+        /// <param name="uploader">Uri recieved in the upload prepare stage</param>
+        /// <param name="callback">Callback that will be invoke upon completion of the upload. Null is sent on request failure</param>
+        public void PerformUpload(Uri uploader, ModelUploadCallback callback)
+        {
+            CapsClient request = new CapsClient(uploader);
+            request.OnComplete += (client, result, error) =>
+            {
+                if (error != null || result == null || result.Type != OSDType.Map)
+                {
+                    Console.WriteLine("Mesh upload request failure {0}", error.Message);
+                    if (callback != null) callback(null);
+                    return;
+                }
+                OSDMap res = (OSDMap)result;
+                if (Debug > 2) Console.WriteLine("Response from mesh upload perform:\n{0}", OSDParser.SerializeLLSDNotationFormatted(result));
+                if (callback != null) callback(res);
+            };
+
+            OSD resources = AssetResources(UploadTextures);
+            if (Debug > 2) Console.WriteLine("Sending Request Resources: {0}", OSDParser.SerializeLLSDNotationFormatted(resources));
+            request.BeginGetResponse(resources, OSDFormat.Xml, 60 * 1000);
+        }
+    }
+}
